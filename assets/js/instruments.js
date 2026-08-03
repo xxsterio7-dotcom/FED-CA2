7e0d337508970e53c184a35745a3a05ade233d6c
MarcoLimJY2302 <110211506+MarcoLimJY2302@users.noreply.github.com>
2026-08-03 21:21:15 +0800

    Update instruments.js

--- assets/js/instruments.js
+++ assets/js/instruments.js
@@ -1,1 +1,11 @@
+const instrCards = document.querySelectorAll(".instrCard");
 
+instrCards.forEach(function(card) { //Loops through all 4 cards
+    card.addEventListener("mouseenter", function() { //Flip when hover
+        card.classList.add("flipped");
+    });
+
+    card.addEventListener("mouseleave", function() { //Remove flip effect when cursor leaves
+        card.classList.remove("flipped");
+    });
+});
